73aa9c204d3a4968e5e1ee915df1eef2d96decdc
Piter <piternek@live.com.mx>
2013-10-06 21:08:51 -0500

    segunda entrega

--- Administrador/js/validaCiclo.js
+++ Administrador/js/validaCiclo.js
@@ -1,1 +1,35 @@
-alert("a");
+jQuery(document).ready(function($){
+	var DIV= document.createElement("div");
+
+	//valida en cada elemento individualmente//por el momento solo elimina los mensajes de error
+	
+	$('#ciclo').focus(function(event) {
+		
+		$(".error").remove();
+	});
+	//---------------
+
+
+
+
+	// valida cuando se da click en el boton de enviar
+	$(".enviar").click(function (e) {
+
+		//e.preventDefault();//para evitar que se envie antes de validar
+		//si ya hay mensajes de error se borran para validar de nuevo
+		$(".error").remove();
+
+		if($("#ciclo").val()=="")
+		{		
+			$(DIV).text("Ingresa un cliclo escolar")
+					.addClass("error")
+			    	.clone().insertAfter($("#ciclo"))
+		}
+
+		
+	});
+
+
+
+});
+
